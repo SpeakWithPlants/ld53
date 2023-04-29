2331d5d90f1640e5a3e7d0577fc4f33f254e8b7d
SpeakWithPlants <speakwithplants@gmail.com>
2023-04-29 17:29:07 -0400

    Initial commit

---
+++ scenes/level.gd
@@ -0,0 +1,91 @@
+extends Node2D
+
+const house_space_y_min = 600
+const house_space_y_max = 1200
+const house_space = 100
+const tree_space = 60
+const fence_height = 32
+const fence_scn = preload("res://scenes/fence.tscn")
+const tree_scn = preload("res://scenes/tree.tscn")
+const house_scn = preload("res://scenes/house.tscn")
+
+var noise
+var course_width
+var course_length
+
+
+func _ready():
+	noise = FastNoiseLite.new()
+	noise.noise_type = FastNoiseLite.TYPE_PERLIN
+	generate_level(10, 2.0)
+	pass
+
+
+func generate_level(houses, tree_density):
+	var view_width = ProjectSettings.get_setting("display/window/size/viewport_width")
+	var view_scale = ProjectSettings.get_setting("display/window/stretch/scale")
+	var scaled_width = view_width / view_scale
+	course_width = scaled_width * 2.5
+	_generate_houses(houses)
+	_generate_fences()
+	_generate_trees(tree_density)
+	pass
+
+
+func _generate_houses(houses):
+	var last_y = 0
+	for i in range(houses):
+		var house_y = last_y + randf_range(house_space_y_min, house_space_y_max)
+		last_y = house_y
+		var house_x_min = course_width / 2.0 - house_space
+		var house_x = randf_range(-house_x_min, house_x_min)
+		_add_house_at(Vector2(house_x, house_y))
+		pass
+	course_length = last_y + house_space_y_max
+	pass
+
+
+func _generate_fences():
+	var current_height = 0
+	while current_height < course_length:
+		_add_fence_at(Vector2.LEFT * course_width / 2.0 + Vector2.DOWN * current_height)
+		_add_fence_at(Vector2.RIGHT * course_width / 2.0 + Vector2.DOWN * current_height)
+		current_height += fence_height
+	pass
+
+
+func _generate_trees(tree_density):
+	var x_start = -course_width / 2.0 + tree_space
+	var x_end = course_width / 2.0 - tree_space
+	for x in range(x_start, x_end, tree_space):
+		for y in range(0, course_length - house_space, tree_space * 2.0):
+			var noise_at_pos = noise.get_noise_2d(x, y)
+			var spawn_chance = noise_at_pos * tree_density
+			if randf() < spawn_chance:
+				var offset = Vector2.RIGHT.rotated(TAU * randf()) * tree_space / 2.0
+				_add_tree_at(Vector2(x, y) + offset)
+	pass
+
+
+func _add_house_at(pos):
+	var new_house = house_scn.instantiate()
+	new_house.global_position = pos
+	add_child(new_house)
+	pass
+
+
+func _add_fence_at(pos):
+	var new_fence = fence_scn.instantiate()
+	new_fence.global_position = pos
+	add_child(new_fence)
+	pass
+
+
+func _add_tree_at(pos):
+	for house in get_tree().get_nodes_in_group("houses"):
+		if house.global_position.distance_to(pos) < house_space * 1.5:
+			return
+	var new_tree = tree_scn.instantiate()
+	new_tree.global_position = pos
+	add_child(new_tree)
+	pass
---
+++ scenes/player.gd
@@ -0,0 +1,98 @@
+extends CharacterBody2D
+
+const max_speed = 850.0
+const gravity = 800.0
+const friction = 100.0
+const control = 500.0
+const turn_speed = 4.5
+
+var use_mouse = false
+var direction = Vector2.RIGHT
+
+@onready var sprite = $sprite
+@onready var trail = $sprite/trail
+@onready var shred = $sprite/shred
+@onready var speed_label = $speed_label
+
+func _ready():
+	trail.emitting = false
+	shred.emitting = false
+	pass
+
+
+func _process(_delta):
+	_update_visual()
+	pass
+
+
+func _physics_process(delta):
+	_update_direction(delta)
+	_update_velocity(delta)
+	move_and_slide()
+	pass
+
+
+func _update_visual():
+	sprite.rotation = direction.angle()
+	speed_label.text = "%0.1f" % velocity.length()
+	_update_trail()
+
+
+func _update_direction(delta):
+	var target_direction = get_target_direction()
+	if target_direction == Vector2.ZERO:
+		target_direction = direction
+	if use_mouse:
+		direction = target_direction
+		return
+	if target_direction == Vector2.ZERO:
+		return
+	var angle_diff = direction.angle_to(target_direction)
+	var frame_rotation = sign(angle_diff) * turn_speed * delta
+	if abs(angle_diff) < (turn_speed * delta):
+		frame_rotation = angle_diff
+	direction = direction.rotated(frame_rotation)
+	pass
+
+
+func _update_velocity(delta):
+	var downhill_dot = direction.dot(Vector2.DOWN)
+	var downhill_multiplier = sign(downhill_dot) * sqrt(abs(downhill_dot))
+	var friction_multiplier = 1.0 - max(0.0, downhill_dot)
+	var max_speed_multiplier = (downhill_dot + 1.0) / 2.0
+	var speed = velocity.length()
+	var current_max_speed = max_speed * max_speed_multiplier
+	speed += gravity * downhill_multiplier * delta
+	speed = move_toward(speed, 0, friction_multiplier * friction * delta)
+	speed = clamp(speed, -current_max_speed, current_max_speed)
+	velocity = velocity.move_toward(direction * speed, control * delta)
+	pass
+
+
+func _update_trail():
+	var speed = velocity.length()
+	trail.emitting = speed > 0
+	shred.emitting = abs(velocity.normalized().dot(direction)) < 0.8 and trail.emitting
+	pass
+
+
+func get_target_direction():
+	var new_target_direction = Vector2.ZERO
+	if Input.is_action_pressed("ui_down"):
+		use_mouse = false
+		new_target_direction.y += 1
+	if Input.is_action_pressed("ui_left"):
+		use_mouse = false
+		new_target_direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		use_mouse = false
+		new_target_direction.x += 1
+	if Input.is_action_pressed("ui_up"):
+		use_mouse = false
+		new_target_direction.y -= 1
+	if Input.is_action_pressed("mouse_left"):
+		use_mouse = true
+		var mouse_pos = get_global_mouse_position()
+		var angle_to_mouse = global_position.angle_to_point(mouse_pos)
+		new_target_direction = Vector2.RIGHT.rotated(angle_to_mouse)
+	return new_target_direction.normalized()
